488caf72f8d9a523840f3fdb81a4541e9451f01d
Gabriela Bohorquez <gaby030300@hotmail.com>
2022-12-07 19:30:45 -0500

    Change Player
    Change Player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float speed;    
     private Vector2 move, mouseLook;
     private Vector3 rotationTarget;
+    Rigidbody rb;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -17,16 +20,14 @@ public class PlayerController : MonoBehaviour
     {
         mouseLook = context.ReadValue<Vector2>();
     }
-    
-    void Update()
-    {            
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(mouseLook);
-        if(Physics.Raycast(ray, out hit))
-            {
-                rotationTarget = hit.point;
-            }
-        MovePlayerWithAim();
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>(); 
+    }
+
+    void FixedUpdate()
+    {
+        FollowMouseLook();       
     }
 
     public void MovePlayerWithAim()
@@ -35,11 +36,23 @@ public class PlayerController : MonoBehaviour
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
         Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
-        if(aimDirection != Vector3.zero)
+        if (aimDirection != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
-        }
+        }        
         Vector3 movement = new Vector3(move.x, 0f, move.y);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        //transform.Translate(movement, Space.World);        
+
+    }
+    public void FollowMouseLook()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+        if (Physics.Raycast(ray, out hit))
+        {
+            rotationTarget = hit.point;
+        }
+        MovePlayerWithAim();
     }
 }
