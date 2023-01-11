0d0e013894206b7fee158c6a955ab70f916a9098
Gabriela Bohorquez <gaby030300@hotmail.com>
2023-01-11 18:00:42 -0500

    UI
    UI

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    public AudioSource audioSource;
+
+    public Slider controlVolume;
+
+    public GameObject[] audios;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audios = GameObject.FindGameObjectsWithTag("Audio");
+        controlVolume.value = PlayerPrefs.GetFloat("volumenSave", 1f);
+
+    }
+    private void Update()
+    {
+        foreach (GameObject au in audios)
+            au.GetComponent<AudioSource>().volume = controlVolume.value;
+    }
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("volumenSave", controlVolume.value);
+    }
+}
