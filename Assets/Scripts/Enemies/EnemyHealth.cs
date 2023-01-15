bc4731566a6177783991ae80c03e677cbac9978c
Daniel Torres <dmtm1404@gmail.com>
2023-01-15 15:58:53 -0500

    Zombie Animation Hurt

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -9,8 +9,12 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] CapsuleCollider capsuleCollider;
     Animator anim;
     public bool isAlive;
+
+    [SerializeField] AudioClip soundHurt, soundDie;
+    AudioSource audioSource;
     private void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         isAlive = true;
         anim = GetComponent<Animator>();
         capsuleCollider = GetComponent<CapsuleCollider>();
@@ -19,19 +23,38 @@ public class EnemyHealth : MonoBehaviour
 
     public void RestHealt(int healtToLoss)
     {
-        if (currentHealt>0)
+        if (currentHealt > 0)
         {
+            StartCoroutine(AnimationDamage());
+            audioSource.PlayOneShot(soundHurt);
             currentHealt -= healtToLoss;
+        }
+        else
+        {
             if (currentHealt <= 0)
                 EnemyDie();
         }
+    }
+
+    IEnumerator AnimationDamage()
+    {
+        anim.SetBool("TakingDamage", true);
+        yield return new WaitForSeconds(0.5f);
+        anim.SetBool("TakingDamage", false);
     }
 
     public void EnemyDie()
     {
         isAlive = false;
         capsuleCollider.enabled = false;
+        audioSource.PlayOneShot(soundDie);
         anim.SetTrigger("Die");
+        Invoke("TurnOffSound",1f);
         //gameObject.SetActive(false);
+    }
+
+    public void TurnOffSound()
+    {
+        audioSource.volume = 0f;
     }
 }
